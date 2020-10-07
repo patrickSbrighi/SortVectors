78fba340f81b1da081bde4f5bf0f98567c79b092
Patrick Sbrighi <PATRICK.SBRIGHI@local.ittcesena.it>
2020-10-07 11:07:00 +0200

    First Step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[10] { 4, -9, 4, 9, 3, -2, 7, 5, 987, 1052 };
+            
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
